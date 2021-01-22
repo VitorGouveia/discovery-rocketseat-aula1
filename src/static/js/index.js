0c049a979fbca81f43eb2fb70b5d0ecc68e65b25
VitorGouveia <vitor.gouevia10@gmail.com>
2021-01-22 14:44:55 -0300

    Nova Feature: Theme Switcher

--- src/static/js/index.js
+++ src/static/js/index.js
@@ -7,9 +7,49 @@ if("serviceWorker" in navigator) {
 }
 
 document.querySelector(".new").addEventListener("click", () => {
-    document.querySelector(".modal-overlay").classList.toggle("active")
+    document.querySelector(".form-modal").classList.toggle("active")
 })
 
-document.querySelector(".cancel").addEventListener("click", () => {
-    document.querySelector(".modal-overlay").classList.toggle("active")
+document.querySelector("#form-button").addEventListener("click", () => {
+    document.querySelector(".form-modal").classList.toggle("active")
 })
+
+document.querySelector("#settings").addEventListener("click", () => {
+    document.querySelector(".settings-modal").classList.toggle("active")
+})
+
+document.querySelector("#settings-button").addEventListener("click", () => {
+    document.querySelector(".settings-modal").classList.toggle("active")
+})
+
+document.querySelector(".settings-modal button").addEventListener("click", () => {
+    document.querySelector(".settings-modal").classList.toggle("active")
+
+    if(document.querySelector("#theme").selectedIndex == 0) {
+        setTheme("light")
+    } else {
+        setTheme("omni")
+    }
+})
+
+function setTheme(themeName) {
+    localStorage.setItem("theme", themeName);
+    document.documentElement.className = themeName;
+}
+
+function toggleTheme() {
+    if (localStorage.getItem("theme") === "theme-dark") {
+        setTheme("light")
+    } else {
+        setTheme("omni")
+    }
+}
+
+(function() {
+    if (localStorage.getItem("theme") === "omni") {
+        setTheme("omni")
+        
+    } else {
+        setTheme("light")
+    }
+})()
